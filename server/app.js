3a2aef48646fb78e21b6661d0892b9c109dfd5e3
taehotaeho <jackt0506@naver.com>
2026-02-10 18:05:25 +0900

    초기 세팅

--- server/app.js
+++ server/app.js
@@ -2,3 +2,14 @@ const express = require("express");
 const { getConnection, oracledb } = require("./db");
 const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
